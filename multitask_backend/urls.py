f82512195b64dcaae71842bdcfe3d652dfde1617
Deekshitha <gattigorladeekshitha@gmail.com>
2026-05-06 21:24:30 +0530

    Add health check test route

--- multitask_backend/urls.py
+++ multitask_backend/urls.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
+from django.http import HttpResponse
+
+def test(request):
+    return HttpResponse("Django is Alive!")
+
 urlpatterns = [
+    path('test/', test),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
